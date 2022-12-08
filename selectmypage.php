5850cb49a76f91fd6e2ad6a2c7bc4d2132397d1d
shouhei <shohei.nagamura@gmail.com>
2022-12-08 16:05:06 +0900

    マイページアイコンの設定

---
+++ selectmypage.php
@@ -0,0 +1,17 @@
+<?php
+include('./functions/check_session_id');
+
+session_start();
+
+$id = $_SESSION['id'];
+$is_user = $_SESSION['is_user'];
+
+if ($is_user == 0) {
+    order_check_session_id();
+    header('Location:./mypageOrder.php');
+} elseif ($is_user == 1) {
+    seller_check_session_id();
+    header('Location:./mypageSeller.php');
+} else {
+    header('Location:./index.php');
+}
